3830ffe5eed260e3ec50a5880577975574b1d7b4
Onyiana <adaurecananaba@gmail.com>
2022-06-28 21:21:56 +0100

    initial commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 113 or i == 101:
-        continue
-    else:
-        print("{}".format(chr(i)), end="")
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
